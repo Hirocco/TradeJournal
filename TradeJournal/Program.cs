3f249b115ff6c492016b7a9cedf057c971078e51
Kamil <futuredev59@gmail.com>
2024-09-05 13:16:13 +0200

    Config

--- TradeJournal/Program.cs
+++ TradeJournal/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using TradeJournal.Data;
+using TradeJournal.Services.token;
+using TradeJournal.Services.user;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +13,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+// JWT
+builder.Services.AddScoped<IJwtTokenService>(provider =>
+    new JwtService(
+        builder.Configuration["Jwt:Key"],
+        builder.Configuration["Jwt:Issuer"],
+        builder.Configuration["Jwt:Audience"],
+        int.Parse(builder.Configuration["Jwt:ExpiryMinutes"])));
+
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBaFt5QHFqVkNrXVNbdV5dVGpAd0N3RGlcdlR1fUUmHVdTRHRbQlVjQX5Uc0JiUHpaeHc=;Mgo+DSMBPh8sVXJyS0d+X1RPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9nSXZTfkVkXHhfeXFTQmA=;Mgo+DSMBMAY9C3t2U1hhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5bdk1jXnpZcX1QQGRc;MzQyMzA3NUAzMjM2MmUzMDJlMzBPb3F5bU10SHhja2R4L0pqOGtkOFVWdnhBbitTQzd0bTBnaTBEUTRmUmRFPQ==");
 builder.Services.AddRazorPages();
